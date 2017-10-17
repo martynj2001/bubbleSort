14e42302de13cc8137bef2d5282156c2c05df555
Martyn Jones (iPad) <martynj2010@icloud.com>
2017-10-17 15:45:30 +0100

    bubbleSortby added

---
+++ bubbleSortBy.rb
@@ -0,0 +1,27 @@
+# #bubble_sort_by => A bubble sort function that takes a block as well
+# as the array to be sorted. The block (can be anything) but in this 
+# case will describe what action to take on the current to array 
+# elements (self[i] and self[i + 1]
+
+def bubble_sort_by arrayTosort
+	
+	swappedflag = true
+	arraySize = arrayTosort.length
+	
+	while flag do
+		swappedflag = false
+		(arraySize - 1).times do |i|
+			if yield(arrayTosort[i], arrayTosort[i + 1])
+				arrayTosort[i], arrayTosort[i + 1] = arrayTosort[i + 1], arrayTosort[i]		
+				swappedflag = true
+			end
+		end		
+	end
+	arrayTosort
+end
+	
+bubblesort([1,4,2,3]){|left, right| left > right}
+bubblesort([“yes”, “very large string”, “no”, “bigger”, “small”, “a”]) do |left, right| 
+	left.length right.length
+end
+			
